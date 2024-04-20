80c03a3298feebefddc651ba00c191ac27d44559
Christian Esparza <cesparzajp@gmail.com>
2024-04-20 20:29:47 +0900

    Add dropdown menu to show services
    I use a 'mouseover' event listener to show a dropdown menu. I decided
    to keep the dropdown menu and the element 'servicios' that triggers the
    event separated in order to maintain the existing document flow. To
    hide the dropdown menu, I added a 'mouseout' event to the dropdown
    itself. This cause an undesired behavior, if accidentally the user
    hovered over 'servicios' then the dropdown would stay visible, if the
    mouse never hovered over it.
    
    To fix this, I used a wrapper that slightly overlaps with 'servicios'.
    Then I added both the 'mouseover' and 'mouseout' events to both,
    'servicios' and the wrapper. This allows the dropdown to stay visible
    when the mouse crosses from 'servicios' and prevents it to remain
    visible if the user accidentally hovers over the 'servicios' tab.

--- scripts/base.js
+++ scripts/base.js
@@ -1,3 +1,15 @@
+const menuKeys = {
+    "index": 0,
+    "nutricion-clinica": 1,
+    "control-de-peso": 1,
+    "nutricion-deportiva": 1,
+    "planes-entrenamiento": 1,
+    "diabetes": 1,
+    "cineantropometria": 1,
+    "precios": 2,
+    "dr-libreros":3,
+    "contacto": 4, 
+}
 const menuItems = document.querySelectorAll(".menu-item");
 
 menuItems.forEach(item => item.addEventListener('click',pageRedirect));
@@ -33,3 +45,52 @@ function getBaseUrl(){
     }
     return baseUrl;
 }
+
+//Highlight active page on menu bar
+
+function getActivePage(){
+
+    const currentUrl = window.location.href; //Fetch the current url
+
+    let pageName = currentUrl.substring(currentUrl.lastIndexOf('/')+1, currentUrl.length - 5); //Remove .html extension
+    const activeMenuItem = menuItems[menuKeys[pageName]];
+    activeMenuItem.classList.add("active");
+
+}
+
+getActivePage();
+
+
+//Display drop-down menu
+
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const dropdownContainer = document.querySelector('.dropdown-container');
+
+
+menuItems[1].addEventListener('mouseover', () => {
+    dropdownContainer.classList.add('show');
+    dropdownMenu.classList.add('show');
+});
+
+//Since the dropdown menu is a separate element, we add the class show
+//again, when the mouse enters the dropdown menu container
+//Both elements slightly overlap making this transition possible
+dropdownContainer.addEventListener('mouseover', () => {
+    dropdownContainer.classList.add('show');
+    dropdownMenu.classList.add('show');
+});
+
+// We remove the class show whenever the mouse leaves the menu item or the dropdown container
+menuItems[1].addEventListener('mouseout', () => {
+    dropdownContainer.classList.remove('show');
+    dropdownMenu.classList.remove('show');
+});
+
+dropdownContainer.addEventListener('mouseout', (event) => {
+    //use related target to select the p elements inside the dropdown menu div
+    //if we hover over any of these p elements, prevent the mouse out event to remove the class 'show'
+    if (!event.relatedTarget || !dropdownMenu.contains(event.relatedTarget)) {
+        dropdownContainer.classList.remove('show');
+        dropdownMenu.classList.remove('show');
+    }
+});
