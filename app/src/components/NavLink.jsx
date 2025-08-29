6af8e6eb648be0ccbe7b3e407806951b9a381fee
Durounseki <156300780+Durounseki@users.noreply.github.com>
2025-08-29 17:38:53 +0900

    feat: style header and footer

--- app/src/components/NavLink.jsx
+++ app/src/components/NavLink.jsx
@@ -1,6 +1,12 @@
 import { Link } from "@tanstack/react-router";
+import styles from "./Header.module.css";
+
 function NavLink({ to, children }) {
-  return <Link to={to}>{children}</Link>;
+  return (
+    <Link to={to} activeProps={{ className: styles.active }}>
+      {children}
+    </Link>
+  );
 }
 
 export default NavLink;
