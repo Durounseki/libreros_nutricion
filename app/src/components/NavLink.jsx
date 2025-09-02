d94bd407f4da66afef7cf6fca6ba8edafb8f9c5a
Durounseki <156300780+Durounseki@users.noreply.github.com>
2025-09-02 20:25:43 +0900

    fix: active link

--- app/src/components/NavLink.jsx
+++ app/src/components/NavLink.jsx
@@ -3,7 +3,11 @@ import styles from "./Header.module.css";
 
 function NavLink({ to, children }) {
   return (
-    <Link to={to} activeProps={{ className: styles.active }}>
+    <Link
+      to={to}
+      activeOptions={{ exact: true }}
+      activeProps={{ className: styles.active }}
+    >
       {children}
     </Link>
   );
