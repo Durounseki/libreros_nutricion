d305a7f4a9237e20d08def2ce9a99f901f52abca
Durounseki <156300780+Durounseki@users.noreply.github.com>
2025-10-08 19:34:09 -0600

    chore: seo setup

--- app/src/routes/contacto.jsx
+++ app/src/routes/contacto.jsx
@@ -131,7 +131,7 @@ function RouteComponent() {
           className={styles["google-map"]}
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.5569754852777!2d-99.16598011746828!3d19.388326395395147!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff002e551c33%3A0x7fa3a3d9bbbd8b66!2sAlfonso%20Libreros%20Nutri%C3%B3logo!5e0!3m2!1sen!2sjp!4v1757071990912!5m2!1sen!2sjp"
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </section>
 
